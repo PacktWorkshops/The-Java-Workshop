72d14ed9e34bab43c34579d7e57e62e21cd718bb
Eric FJ <ericfoster-johnson1@Erics-iMac.local>
2019-08-25 13:04:38 -0500

    Fix loop end count

--- Chapter14/chapter14/src/main/java/com/packtpub/recursion/Fibonacci.java
+++ Chapter14/chapter14/src/main/java/com/packtpub/recursion/Fibonacci.java
@@ -15,7 +15,7 @@ public class Fibonacci {
 
     public static void main(String[] args) {
 
-        for (int i = 0; i < 17; i++) {
+        for (int i = 0; i < 15; i++) {
             System.out.println(fibonacci(i));
         }
     }
