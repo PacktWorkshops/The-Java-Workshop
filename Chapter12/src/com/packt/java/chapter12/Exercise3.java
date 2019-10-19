3121eb7035e79b3f7b802fda4a31cdbccd71511f
rutuja027 <40690593+rutuja027@users.noreply.github.com>
2019-10-19 09:53:09 +0530

    Update Exercise3.java

--- Chapter12/src/com/packt/java/chapter12/Exercise3.java
+++ Chapter12/src/com/packt/java/chapter12/Exercise3.java
@@ -12,22 +12,16 @@ import java.util.regex.Pattern;
 public class Exercise3 {
 
     public static void main(String[] args) {
-
         String filePath = System.getProperty("user.dir") + File.separator +  "res" + File.separator + "packt.txt";
         try {
             String packtDump = new String(Files.readAllBytes(Paths.get(filePath)));
-
             String regex = "(?:<a href=\")([^\"]+)(?:\"{1})";
-
             Pattern pattern = Pattern.compile(regex);
             Matcher matcher = pattern.matcher(packtDump);
-
             List<String> links = new ArrayList<>();
-
             while (matcher.find()) {
                 links.add(matcher.group(1));
             }
-
             System.out.println(links);
         } catch (IOException e) {
             e.printStackTrace();
