da6a5acf95639fe9d44d2ebca6e7d2ab9e7f884c
ManasaVk243 <manasak@packtpub.com>
2019-12-06 14:45:55 +0530

    Update Exercise03.java

--- Chapter02/Exercise03.java
+++ Chapter02/Exercise03.java
@@ -1,14 +1,14 @@
 public class Exercise03 {
     public static void main(String[] args) {
-        int speed = 55;
+        int speed = 75;
         int maxSpeed = 60;
+        int speedForFine = 70;
 
         // Nested if statements.
-        if (speed <= maxSpeed) {
-            System.out.println("Speed is less than or equal to the max. speed limit");
-
-            if (speed < maxSpeed) {
-                System.out.println("Speed is less than the max. speed limit");
+        if (speed >= maxSpeed) { 
+            System.out.println("You're over the speed limit"); 
+        if (speed >= speedForFine) { 
+            System.out.println("You are eligible for a fine!"); 
             }
         }
     }
