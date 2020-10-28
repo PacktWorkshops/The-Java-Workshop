8b34de2d77acada7cbee62c0d0078ad173945106
dcuartielles <dcuartielles@gmail.com>
2020-10-28 13:39:08 +0100

    added data types as per reader's request
    added data types as per reader's request

--- Chapter04/Example11/Example11.java
+++ Chapter04/Example11/Example11.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Example11 {
     public static void main(String[] args) {
-        Set set1 = new LinkedHashSet();
+        Set<Integer> set1 = new LinkedHashSet<>();
 
         set1.add(35);
         set1.add(19);
@@ -10,7 +10,7 @@ public class Example11 {
         set1.add(83);
         set1.add(7);
 
-        Set set2 = new LinkedHashSet();
+        Set<Integer> set2 = new LinkedHashSet<>();
 
         set2.add(3);
         set2.add(19);
