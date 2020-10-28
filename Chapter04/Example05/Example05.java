0a8c399e81cb3ac13e053b4bd0f120faa9dabcbc
dcuartielles <dcuartielles@gmail.com>
2020-10-28 10:43:12 +0100

    added type of data in the set
    While it is possible to declare the set without any type declaration, we choose to add the type explictly to avoid confusion among readers. Fixed after a reader's question about the warnings from the compiler.

--- Chapter04/Example05/Example05.java
+++ Chapter04/Example05/Example05.java
@@ -4,7 +4,7 @@ public class Example05 {
     public static void main(String[] args) {
         Integer[] myArray = new Integer[] {3, 25, 2, 79, 2};
 
-        Set mySet = new HashSet(Arrays.asList(myArray));
+        Set<Integer> mySet = new HashSet<>(Arrays.asList(myArray));
 
         System.out.println(mySet);
     }
