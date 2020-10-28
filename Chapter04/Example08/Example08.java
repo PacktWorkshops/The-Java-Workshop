0e5b62c1388d4e5ec90397416d915c086d232f63
dcuartielles <dcuartielles@gmail.com>
2020-10-28 11:09:47 +0100

    added type of data upon reader's request
    added type of data upon reader's request

--- Chapter04/Example08/Example08.java
+++ Chapter04/Example08/Example08.java
@@ -5,10 +5,10 @@ public class Example08 {
         Integer[] numbers1 = new Integer[] {3, 25, 2, 79, 2};
         Integer[] numbers2 = new Integer[] {7, 12, 14, 79};
 
-        Set set1 = new HashSet();
+        Set<Integer> set1 = new HashSet<>();
         Collections.addAll(set1, numbers1);
 
-        Set set2 = new HashSet();
+        Set<Integer> set2 = new HashSet<>();
         Collections.addAll(set2, numbers2);
 
         set1.addAll(set2);
