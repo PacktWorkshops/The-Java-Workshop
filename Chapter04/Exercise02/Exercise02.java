e18eb28d4ee5f98fd5765cff7381b1b81ff0b5fd
dcuartielles <dcuartielles@gmail.com>
2020-10-28 14:49:29 +0100

    revised the definition of types of containers
    revised the definition of types of containers

--- Chapter04/Exercise02/Exercise02.java
+++ Chapter04/Exercise02/Exercise02.java
@@ -3,14 +3,14 @@ import java.io.Console;
 
 public class AnalyzeInput {
     public static void main(String[] args) {
-        ArrayList <String> text = new ArrayList <String> ();
+        ArrayList <String> text = new ArrayList <> ();
         Console cons;
         String line = "";
 
         while (!line.equals("*")
                 && (cons = System.console()) != null
                 && (line = cons.readLine()) != null) {
-            List <String> lineList = new ArrayList <String> (Arrays.
+            List <String> lineList = new ArrayList <> (Arrays.
 asList(line.split(" ")));
             text.addAll(lineList);
         }
@@ -18,7 +18,7 @@ asList(line.split(" ")));
         System.out.println("You typed: " + text);
         System.out.println("Word count: " + text.size());
 
-        Set <String> textSet = new HashSet <String> ();
+        Set <String> textSet = new HashSet <> ();
         textSet.addAll(text);
 
         Iterator iterator = textSet.iterator();
