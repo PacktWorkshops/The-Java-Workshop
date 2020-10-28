ff7b4bfb6bd1b744b02be5b0e77f0914cf06644d
dcuartielles <dcuartielles@gmail.com>
2020-10-28 10:57:32 +0100

    changed the name of the counting variable
    To satisfy a reader's comment, I changed the name of the counting variable from "occurrence" to "occurrences"

--- Chapter04/Activity01/Activity01.java
+++ Chapter04/Activity01/Activity01.java
@@ -4,18 +4,18 @@ public class Activity01 {
     public static void main(String[] args) {
         String[] text = {"So", "many", "books", "so", "little", "time"};
         String toSearch = "so";
-        int occurrence = -1;
+        int occurrences = -1;
 
         for(int i = 0; i < 5; i++) {
             if (text[i].compareToIgnoreCase(toSearch) == 0) {
                 System.out.println("Found query at :" + i);
-                occurrence++;
+                occurrences++;
             } 
         }
 
-        if (occurrence > -1) {
-            occurrence++;
-            System.out.println("Found: " + occurrence + " coinciding words");
+        if (occurrences > -1) {
+            occurrences++;
+            System.out.println("Found: " + occurrences + " coinciding words");
         }
     }
 }
