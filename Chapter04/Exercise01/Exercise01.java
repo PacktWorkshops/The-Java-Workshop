874ef212ddecc29b1ea5d5fed7770823e8f07d8f
Abhishek R <55444296+abhishekr128@users.noreply.github.com>
2020-11-27 20:59:05 +0530

    Update Exercise01.java
    Change Example to Exercise

--- Chapter04/Exercise01/Exercise01.java
+++ Chapter04/Exercise01/Exercise01.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.Console;
 
-public class Example01 {
+public class Exercise01 {
     public static void main(String[] args) {
         ArrayList <String> text = new ArrayList<>();
         Console cons;
